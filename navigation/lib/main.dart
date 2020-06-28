a04b265e7b6184a6e43dbe6fc212692f67dd0ae9
jmsanchez <info@jmsanchez.eu>
2020-06-28 23:27:39 +0200

    Send data to new screen todo list

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -37,6 +37,9 @@ class MyApp extends StatelessWidget {
         home: FirstScreen(),
         //initialRoute: '/',
         routes: {
+          '/todos': (context) => TodosScreen(
+              todos: List.generate(
+                  20, (i) => Todo('Todo $i', 'A description of todo $i'))),
           '/second': (context) => SecondScreen(),
           ExtractArgumentsScreen.routeName: (context) =>
               ExtractArgumentsScreen(),
@@ -66,12 +69,7 @@ class MyApp extends StatelessWidget {
 class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Route'),
-      ),
-      body: Center(
-        child: RaisedButton(
+    return Scaffold((context) => 
           onPressed: () {
             Navigator.pop(context);
           },
@@ -137,6 +135,11 @@ class FirstScreen extends StatelessWidget {
               },
             ),
             SelectionButton(),
+            RaisedButton(
+                child: Text("Todo list"),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/todos');
+                }),
           ],
         ),
       ),
@@ -289,6 +292,71 @@ class SelectionScreen extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
+
+class TodosScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  //requiring the list of todos
+  TodosScreen({Key key, @required this.todos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+      ),
+      //passing in the ListView.builder
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            // When a user taps the ListTile, navigate to the DetailScreen.
+            // Notice that you're not only creating a DetailScreen, you're
+            // also passing the current todo to it.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  // Declare a field that holds the Todo.
+  final Todo todo;
+
+  // In the constructor, require a Todo.
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
       ),
     );
   }
